3f36df4124916a239bd4902a50f23b54012a794a
Derald D. Woods <woods.technical@gmail.com>
2023-11-03 22:41:11 -0400

    pynq_z2: bump to version xilinx-v2023.2

--- hdl/pynq_z2_wrapper.vhd
+++ hdl/pynq_z2_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 --Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2023.1 (lin64) Build 3865809 Sun May  7 15:04:56 MDT 2023
+--Tool Version: Vivado v.2023.2 (lin64) Build 4029153 Fri Oct 13 20:13:54 MDT 2023
 --Date Removed:
 --Host Removed:
 --Command     : generate_target pynq_z2_wrapper.bd
